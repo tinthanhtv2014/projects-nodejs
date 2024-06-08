6c9221fb032e935f5857be3b210344b6cef1207e
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-06-08 22:56:48 +0700

    update

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const configViewEngine = require("./config/viewEngine");
-
+const mongoose = require("mongoose");
 const app = express();
 const port = 8081;
 const hostname = "localhost";
@@ -13,21 +13,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 configViewEngine(app); //configViewEngine
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-
 //config json api
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //config static
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  return res.sendFile(__dirname + "/index.html");
+const dbUrl = "mongodb://localhost:27017/testabc";
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+  })
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.error("mongodb connection error:", err);
+  });
+
+const userSchema = new mongoose.Schema({
+  // Các trường khác của user
+  email: String,
+  username: String,
 });
 
-io.on("connection", (socket) => {
-  console.log("connection");
+const User = mongoose.model("User", userSchema);
+
+app.post("/createUser", async (req, res) => {
+  try {
+    const newUser = new User({
+      email: req.body.email,
+      username: req.body.phone,
+    });
+    console.log("Check User", req.body);
+    // Lưu user mới vào cơ sở dữ liệu
+    await newUser.save();
+
+    // Phản hồi với mã trạng thái 200 để cho biết tạo user thành công
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    // Phản hồi với mã trạng thái 500 để cho biết có lỗi xảy ra
+    res.sendStatus(500);
+  }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    // Lấy tất cả các người dùng từ cơ sở dữ liệu
+    const users = await User.find();
+    console.log(users);
+    return res.render("home.ejs", { users: users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 app.listen(port, () => {
